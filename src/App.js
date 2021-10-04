b2705daa92b3e9ed31dee49f4221d2b9b9181161
enzoa96 <enzo_vazquez@outlook.com>
2021-10-04 12:21:39 -0300

    imagen de fondo y date

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React from "react";
+import DesktopWindowsIcon from "@mui/icons-material/DesktopWindows";
+
+var today = new Date();
+var date =
+  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+var time =
+  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date + " " + time;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hola Mundo</h1>
+        <div className="bg-black">
+          <h1 className="text-white text-center">{dateTime}</h1>
+          <div>
+            <img
+              src="https://www.xtrafondos.com/wallpapers/ubuntu-linux-minimalism-3445.jpg"
+              height={190}
+            />
+          </div>
+        </div>
       </header>
     </div>
   );
