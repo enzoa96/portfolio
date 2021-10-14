f738ec55b8cf33c471e31136a759911c0485057c
enzoa96 <enzo_vazquez@outlook.com>
2021-10-14 12:36:27 -0300

    views con onclose y x fix

--- src/views/papelera.js
+++ src/views/papelera.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { ImCross } from "react-icons/im";
 
-export default function mipc() {
-  return <h1>hola papelera</h1>;
+export default function Papelera({ onClose }) {
+  return (
+    <div className="flex justify-center -mt-72">
+      <div className="bg-white shadow-2xl rounded-lg w-3/6 h-96">
+        <header className="items-center grid grid-cols-3 bg-gray-300 text-black py-1 px-8 rounded-t-lg text-xl text-center">
+          <span className="col-span-2 justify-self-end">
+            C:\Users\Enzo\Documents
+          </span>
+          <ImCross className="justify-self-end" onClick={onClose} />
+        </header>
+        <h1 className="text-black"> Hola mi papelera </h1>
+      </div>
+    </div>
+  );
 }
