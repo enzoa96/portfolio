1b6d1bb6adff3daab1ddea67016e0b1c8576e53a
enzoa96 <enzo_vazquez@outlook.com>
2021-10-14 12:34:46 -0300

    onClose, acomodado de boton exit y span

--- src/views/archivos.js
+++ src/views/archivos.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { ImCross } from "react-icons/im";
 
-export default function mipc() {
+export default function archivos({ onClose }) {
   return (
     <div className="flex justify-center -mt-72">
       <div className="bg-white shadow-2xl rounded-lg w-3/6 h-96">
-        <header className=" bg-gray-300 text-black py-1 px-8 rounded-t-lg text-xl text-center">
-          C:\Users\Enzo\Documents
+        <header className="items-center grid grid-cols-3 bg-gray-300 text-black py-1 px-8 rounded-t-lg text-xl text-center">
+          <span className="col-span-2 justify-self-end">
+            C:\Users\Enzo\Documents
+          </span>
+          <ImCross className="justify-self-end" onClick={onClose} />
         </header>
-        <h1 className="text-black"> Hola mi rey </h1>
+        <h1 className="text-black"> Hola archivos </h1>
       </div>
     </div>
   );
